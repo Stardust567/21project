c0a561882b71e0d2724769ec7bf37f0ce944bd9d
ChordWiz@swc_20180076 <stardust1084062596@gmail.com>
2019-09-24 10:06:46 +0800

    add 01-softmax

--- 01mnist/softmax.py
+++ 01mnist/softmax.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # 占位符
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 # 变量
 W = tf.Variable(tf.zeros([784, 10]))
+tf.Variable()
 b = tf.Variable(tf.zeros([10]))
 # 计算
 y = tf.nn.softmax(tf.matmul(x, W)+b)
